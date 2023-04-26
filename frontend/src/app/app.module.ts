ff2b65a7819a366e5f596ae98159add08eefd4fd
kadidia98 <diallokhadidiatoubouba@gmail.com>
2023-04-26 10:15:34 +0000

    debut-projet

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocComponent } from './doc/doc.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    DashboardComponent,
+    DocComponent,
+    InscriptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
