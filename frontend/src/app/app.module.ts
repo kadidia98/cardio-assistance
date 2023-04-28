547f6953a98c2e343ffc3dc1078d9da3a8196477
kadidia98 <diallokhadidiatoubouba@gmail.com>
2023-04-28 14:46:12 +0000

    connection et admin

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,9 +21,69 @@ import { InscriptionComponent } from './inscription/inscription.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+export class AppModule { }*/
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { InscriptionComponent } from './inscription/inscription.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginComponent } from './login/login.component';
+
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocComponent } from './doc/doc.component';
+
+
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http'; //intersepteurs
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    AdminComponent,
+ DashboardComponent,
+ DocComponent,
+LoginComponent,
+    InscriptionComponent
+  ],
+  imports: [
+
+    BrowserModule,
+    AppRoutingModule,
+    
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+   
+    
+
+
+  ],
+
+  providers: [
+    
+  ],
+  bootstrap: [AppComponent]
+
+})
 export class AppModule { }
+
