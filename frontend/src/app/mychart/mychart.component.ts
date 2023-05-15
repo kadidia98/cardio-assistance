0968eeebd13f896e6e769fd693f42d4654432d26
Josephine <sosodjibrildiop@gmail.com>
2023-05-15 12:56:48 +0000

    pulse

--- frontend/src/app/mychart/mychart.component.ts
+++ frontend/src/app/mychart/mychart.component.ts
@@ -1,67 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { SocketService } from '../services/socket.service';
-import {Socket, io }from 'socket.io-client';
+import { Socket, io } from 'socket.io-client';
 
 @Component({
   selector: 'app-mychart',
   templateUrl: './mychart.component.html',
   styleUrls: ['./mychart.component.css']
 })
-export class MychartComponent  implements OnInit{
-  public chart: any;
-  temp! :any [];
-  bpm!: any [];
-  donnee:any
-  constructor( private SocketService: SocketService){
-    this.socket = io('ws://localhost:3000');
-  }
+export class MychartComponent implements OnInit {
+  public chart!: Chart;
+  public bpm!: any;
   private socket: Socket;
 
-
-  ngOnInit(): void {
-    //   let tab = ['467','576', '572', '79', '92',
-    //   '574', '573', '576']
-    let tab:any=[];
-      this.SocketService.getTemp().subscribe((data) =>{
-          //console.log(data);
-          this.donnee = data
-          tab.push(this.donnee)
-          this.createChart(tab);
-       console.log("test: ",this.donnee);
-       
-      
-        
-      
-        })
-
+  constructor(private socketService: SocketService) {
+    this.socket = io('ws://localhost:3000');
   }
 
- 
+  ngOnInit(): void {
+    let tab: number[] = [];
 
+    this.socketService.getTemp().subscribe((data) => {
+      this.bpm = data;
+      tab.push(this.bpm);
+      this.updateChart(tab);
+    });
+  }
 
-  createChart(btmnt:any){
-  
+  createChart(bpmData: number[]): void {
+    const canvas = document.getElementById('MyChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
     this.chart = new Chart("MyChart", {
-      type: 'line', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
+      type: 'line',
+      data: {
+        labels: ['1000ms', '2000ms', '3000ms', '4000ms', '5000ms', '6000ms', '7000ms', '8000ms'],
+        datasets: [
           {
-            label: "Sales",
-            data:btmnt,
-            backgroundColor: 'blue'
-          },
-         
+            label: "Pulse",
+            data: bpmData,
+            backgroundColor: 'blue',
+            borderColor: 'blue',
+            fill: false
+          }
         ]
       },
       options: {
-        aspectRatio:2.5
+        aspectRatio: 2.5
       }
-      
     });
+  }
 
+  updateChart(bpmData: number[]): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+    this.createChart(bpmData);
   }
 }
